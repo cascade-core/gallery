9aff8619d08d1a218af23b12d1ef4310c27c7ef9
Josef Kufner <josef@kufner.cz>
2016-12-16 23:28:20 +0100

    Add WebDAV link

---
+++ block/gallery/show_dav_link.php
@@ -0,0 +1,33 @@
+<?php
+/*
+ * Copyright (c) 2011-2014, Josef Kufner  <jk@frozen-doe.net>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+
+class B_gallery__gallery__show_dav_link extends B_core__out__output
+{
+
+	protected $inputs = array(
+		'dav_url' => null,
+		'slot' => 'default',
+		'slot_weight' => 40,
+	);
+
+	protected $template = 'gallery/dav_link';
+
+};
+
+
---
+++ template/html5/dav_link.php
@@ -0,0 +1,38 @@
+<?php
+/*
+ * Copyright (c) 2011-2014, Josef Kufner  <jk@frozen-doe.net>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+
+function TPL_html5__gallery__dav_link($t, $id, $d, $so)
+{
+	extract($d);
+
+	if (empty($dav_url)) {
+		return;
+	}
+
+	echo "<div class=\"gallery_dav_link\">\n";
+	{
+		echo "\tWebDAV access: <a href=\"", htmlspecialchars($dav_url), "\">\n",
+			"<code>", htmlspecialchars($dav_url), "</code>",
+			"</a>\n";
+	}
+	echo "</div>\n";
+}
+
+
+
